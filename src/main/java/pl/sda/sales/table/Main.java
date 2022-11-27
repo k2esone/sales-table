f229dc0aa2dc2ca6291a901b8fea7b42b31744ab
k2esone <dkowalczyk.lwc@gmail.com>
2022-11-27 14:46:33 +0100

    first commit

---
+++ src/main/java/pl/sda/sales/table/Main.java
@@ -0,0 +1,7 @@
+package pl.sda.sales.table;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/sda/sales/table/module/Produkt.java
@@ -0,0 +1,4 @@
+package pl.sda.sales.table.module;
+
+public class Produkt {
+}
