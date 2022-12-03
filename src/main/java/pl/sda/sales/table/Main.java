0df416bd411b464a019577c7127249a743a774df
k2esone <dkowalczyk.lwc@gmail.com>
2022-12-03 09:09:29 +0100

    add methods

--- src/main/java/pl/sda/sales/table/Main.java
+++ src/main/java/pl/sda/sales/table/Main.java
@@ -11,8 +11,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Co chcesz zrobic? " +
                 "\ndodaj produkt " +
@@ -27,178 +28,206 @@ public class Main {
 
 /// DODAJ PRODUKT!
         if (wykonaj.equalsIgnoreCase("dodaj produkt")) {
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                Transaction transaction = session.beginTransaction();
-
-                System.out.println("Podaj kategorie produktu");
-                String kat = scanner.nextLine();
-                Kategoria kategoria = Kategoria.valueOf(kat);
-
-                System.out.println("Podaj nazwe produktu");
-                String nazwa = scanner.nextLine();
-
-                Produkt produkt = Produkt.builder()
-                        .kategoria(kategoria)
-                        .nazwa(nazwa)
-                        .build();
-
-                session.persist(produkt);
-
-                transaction.commit();
-
-            } catch (Exception ioe) {
-
-            }
+            dodajProdukt();
         }
 
 ///DODAJ SPRZEDAZ
         if (wykonaj.equalsIgnoreCase("dodaj sprzedaz")) {
-
-            System.out.println("Podaj id produktu dla ktorego chcesz utworzyc sprzedaz");
-            Long id = Long.parseLong(scanner.nextLine());
-
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                Transaction transaction = session.beginTransaction();
-                Produkt produkt = session.get(Produkt.class, id);
-                if (produkt == null) System.out.println("Nie znaleziono produktu");
-                else {
-                    System.out.println("Podaj cene sprzedazy produktu");
-                    Double cena = Double.parseDouble(scanner.nextLine());
-
-                    System.out.println("Podaj ilosc");
-                    Double ilosc = Double.parseDouble(scanner.nextLine());
-
-                    Sprzedaz sprzedaz = Sprzedaz.builder()
-                            .cena(cena)
-                            .ilosc(ilosc)
-                            .produkt(produkt)
-                            .build();
-
-                    session.persist(sprzedaz);
-                }
-                transaction.commit();
-
-            } catch (Exception ioe) {
-
-            }
-
+            dodajSprzedaz();
         }
 
 ///WYSWIETL LISTE PRODUKTOW
         if (wykonaj.equalsIgnoreCase("wyswietl liste produktow")) {
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                TypedQuery<Produkt> zapytanie = session.createQuery("FROM Produkt", Produkt.class);
-                List<Produkt> lista = zapytanie.getResultList();
-                lista.forEach(System.out::println);
-            } catch (Exception ioe) {
-
-            }
+            wyswietlListeProduktow();
         }
 
 ///WYSWIETL LISTE SPRZEDAZY
         if (wykonaj.equalsIgnoreCase("wyswietl liste sprzedazy")) {
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                TypedQuery<Sprzedaz> zapytanie = session.createQuery("FROM Sprzedaz", Sprzedaz.class);
-                List<Sprzedaz> lista = zapytanie.getResultList();
-                lista.forEach(System.out::println);
-            } catch (Exception ioe) {
-
-            }
+            wyswietlListeSprzedazy();
         }
 
 /// !! DO POPRAWY !!
 ///WYSWIETL LISTE SPRZEDAZY PRODUKTOW
         if (wykonaj.equalsIgnoreCase("wyswietl liste sprzedazy produktu")) {
-            System.out.println("Podaj id produktu, do wyswietlenia listy sprzedazy");
-            int id = Integer.parseInt(scanner.nextLine());
-
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                TypedQuery<Sprzedaz> zapytanie = session.createQuery("FROM Sprzedaz", Sprzedaz.class);
-                List<Sprzedaz> lista = zapytanie.getResultList();
-                System.out.println(lista.get(id));
-
-            } catch (Exception ioe) {
-
-            }
+            wyswietlListeSprzedazyProduktow();
         }
 
 ///USUN SPRZEDAZ
         if (wykonaj.equalsIgnoreCase("usun sprzedaz")) {
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                Transaction transaction = session.beginTransaction();
-
-                System.out.println("Podaj id sprzedazy, ktora chcesz usunac");
-                Long id = Long.parseLong(scanner.nextLine());
-
-                Sprzedaz sprzedaz = session.get(Sprzedaz.class, id);
-                if (sprzedaz == null) {
-                    System.err.println("Nie znaleziono sprzedazy");
-                } else {
-                    session.remove(sprzedaz);
-                }
-                transaction.commit();
-            } catch (Exception ioe) {
-
-            }
+            usunSprzedaz();
         }
 
 ///USUN PRODUKT
         if (wykonaj.equalsIgnoreCase("usun produkt")) {
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                Transaction transaction = session.beginTransaction();
-
-                System.out.println("Podaj id produktu, ktory chcesz usunac");
-                Long id = Long.parseLong(scanner.nextLine());
-
-                Produkt produkt = session.get(Produkt.class, id);
-                if (produkt == null) {
-                    System.err.println("Nie znaleziono produktu");
-                } else {
-                    if (!produkt.getSprzedaze().isEmpty()) {
-                        for (Sprzedaz sprzedaz : produkt.getSprzedaze()) {
-                            session.remove(sprzedaz);
-                        }
-                    }
-                    session.remove(produkt);
-                }
-                transaction.commit();
-
-            } catch (Exception ioe) {
-                System.err.println("Błąd bazy: " + ioe);
-            }
+            usunProdukt();
         }
 
 ///USUN PRODUKT(NULL)
         if (wykonaj.equalsIgnoreCase("usun produkt(null)")) {
-            try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
-                Transaction transaction = session.beginTransaction();
-
-                System.out.println("Podaj id produktu, ktory chcesz usunac");
-                Long id = Long.parseLong(scanner.nextLine());
-
-                Produkt produkt = session.get(Produkt.class, id);
-                if (produkt == null) {
-                    System.err.println("Nie znaleziono produktu");
-                } else {
-                    if (!produkt.getSprzedaze().isEmpty()) {
-                        for (Sprzedaz sprzedaz : produkt.getSprzedaze()) {
-                            sprzedaz.setProdukt(null);
-                        }
-                    }
-                    session.remove(produkt);
-                }
-                transaction.commit();
-
-            } catch (Exception ioe) {
-                System.err.println("Błąd bazy: " + ioe);
-            }
+            usunProduktNull();
         }
 
-
-/// ZROBIC WSZYSTKO W METODACH!!!
 /// POPRAWIC LISTE SPRZEDAZY PRODUKTOW!!!
 /// POPRAWIC SREDNIA WAZONA!!!
 
 
     }
+
+    public static void dodajProdukt() {
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            System.out.println("Podaj kategorie produktu");
+            String kat = scanner.nextLine();
+            Kategoria kategoria = Kategoria.valueOf(kat);
+
+            System.out.println("Podaj nazwe produktu");
+            String nazwa = scanner.nextLine();
+
+            Produkt produkt = Produkt.builder()
+                    .kategoria(kategoria)
+                    .nazwa(nazwa)
+                    .build();
+
+            session.persist(produkt);
+
+            transaction.commit();
+
+        } catch (Exception ioe) {
+
+        }
+    }
+
+    public static void dodajSprzedaz() {
+        System.out.println("Podaj id produktu dla ktorego chcesz utworzyc sprzedaz");
+        Long id = Long.parseLong(scanner.nextLine());
+
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Produkt produkt = session.get(Produkt.class, id);
+            if (produkt == null) System.out.println("Nie znaleziono produktu");
+            else {
+                System.out.println("Podaj cene sprzedazy produktu");
+                Double cena = Double.parseDouble(scanner.nextLine());
+
+                System.out.println("Podaj ilosc");
+                Double ilosc = Double.parseDouble(scanner.nextLine());
+
+                Sprzedaz sprzedaz = Sprzedaz.builder()
+                        .cena(cena)
+                        .ilosc(ilosc)
+                        .produkt(produkt)
+                        .build();
+
+                session.persist(sprzedaz);
+            }
+            transaction.commit();
+
+        } catch (Exception ioe) {
+
+        }
+    }
+
+    public static void wyswietlListeProduktow(){
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            TypedQuery<Produkt> zapytanie = session.createQuery("FROM Produkt", Produkt.class);
+            List<Produkt> lista = zapytanie.getResultList();
+            lista.forEach(System.out::println);
+        } catch (Exception ioe) {
+
+        }
+    }
+
+    public static void wyswietlListeSprzedazy(){
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            TypedQuery<Sprzedaz> zapytanie = session.createQuery("FROM Sprzedaz", Sprzedaz.class);
+            List<Sprzedaz> lista = zapytanie.getResultList();
+            lista.forEach(System.out::println);
+        } catch (Exception ioe) {
+
+        }
+    }
+    public static void wyswietlListeSprzedazyProduktow(){
+        System.out.println("Podaj id produktu, do wyswietlenia listy sprzedazy");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            TypedQuery<Sprzedaz> zapytanie = session.createQuery("FROM Sprzedaz", Sprzedaz.class);
+            List<Sprzedaz> lista = zapytanie.getResultList();
+            System.out.println(lista.get(id));
+
+        } catch (Exception ioe) {
+
+        }
+    }
+
+    public static void usunSprzedaz(){
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            System.out.println("Podaj id sprzedazy, ktora chcesz usunac");
+            Long id = Long.parseLong(scanner.nextLine());
+
+            Sprzedaz sprzedaz = session.get(Sprzedaz.class, id);
+            if (sprzedaz == null) {
+                System.err.println("Nie znaleziono sprzedazy");
+            } else {
+                session.remove(sprzedaz);
+            }
+            transaction.commit();
+        } catch (Exception ioe) {
+
+        }
+    }
+
+    public static void usunProdukt(){
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            System.out.println("Podaj id produktu, ktory chcesz usunac");
+            Long id = Long.parseLong(scanner.nextLine());
+
+            Produkt produkt = session.get(Produkt.class, id);
+            if (produkt == null) {
+                System.err.println("Nie znaleziono produktu");
+            } else {
+                if (!produkt.getSprzedaze().isEmpty()) {
+                    for (Sprzedaz sprzedaz : produkt.getSprzedaze()) {
+                        session.remove(sprzedaz);
+                    }
+                }
+                session.remove(produkt);
+            }
+            transaction.commit();
+
+        } catch (Exception ioe) {
+            System.err.println("Błąd bazy: " + ioe);
+        }
+    }
+
+    public static void usunProduktNull(){
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            System.out.println("Podaj id produktu, ktory chcesz usunac");
+            Long id = Long.parseLong(scanner.nextLine());
+
+            Produkt produkt = session.get(Produkt.class, id);
+            if (produkt == null) {
+                System.err.println("Nie znaleziono produktu");
+            } else {
+                if (!produkt.getSprzedaze().isEmpty()) {
+                    for (Sprzedaz sprzedaz : produkt.getSprzedaze()) {
+                        sprzedaz.setProdukt(null);
+                    }
+                }
+                session.remove(produkt);
+            }
+            transaction.commit();
+
+        } catch (Exception ioe) {
+            System.err.println("Błąd bazy: " + ioe);
+        }
+    }
+
 }
